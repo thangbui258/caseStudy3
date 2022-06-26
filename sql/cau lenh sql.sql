0c7d0e1bc1dd8ccc0af2f575a7d7bdbf4f7bb172
thangbui258 <thangbuimanh1996@gmail.com>
2022-06-26 13:28:44 +0700

    xong

---
+++ sql/cau lenh sql.sql
@@ -0,0 +1,54 @@
+create database QuanLyHS;
+use QuanLyHS;
+
+--tao bang UserLogin
+create table UserLogin (
+	Name varchar(40) not null,
+    Email varchar(60) not null unique primary key,
+    Pass varchar(16) not null
+);
+alter table UserLogin add Roll varchar(10) not null;
+--tao bang danh sach hoc sinh
+create table DanhSachHS (
+	ID_HS int(4) not null auto_increment primary key,
+    Name varchar(40) not null,
+    Email varchar(60) not null unique,
+    Phone int(10) not null
+    );
+    
+--tao bang danh sach mon hoc
+create table Subject (
+	ID_subject int(2) not null auto_increment primary key,
+    Subject varchar(26) not null
+);
+
+--tao bang ket qua
+create table KetQua (
+	ID_HS int(4) not null,
+    ID_subject int(2) not null,
+    Score int(2),
+    primary key(ID_HS,ID_subject),
+    foreign key (ID_HS) references DanhSachHS(ID_HS),
+    foreign key (ID_subject) references Subject(ID_subject)
+);
+
+--tao ban ghi Userlogin
+ insert into UserLogin (Name,Email,Pass,Roll) values ('admin', 'admin@gmail.com',123,'admin');
+ insert into UserLogin (Name,Email,Pass,Roll) values ('thang', 'thang@gmail.com',123,'member');
+
+ insert into DanhSachHS (Name,Email,Phone) values ('Nam', 'nam@gmail.com',1234567890);
+ insert into DanhSachHS (Name,Email,Phone) values ('Minh', 'Minh@gmail.com',1234567890);
+ 
+ insert into Subject (Subject) values ('Toan');
+ insert into Subject (Subject) values ('Van');
+ insert into Subject (Subject) values ('Anh');
+ 
+delimiter //
+CREATE PROCEDURE delete_list_student (IN id INT)
+  BEGIN
+    delete from KetQua where ID_HS = id;
+	delete from DanhSachHS where ID_HS = id;
+  END//
+delimiter ; 
+CALL delete_list_student(2);
+
