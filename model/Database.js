e8cc572b2ddade97995ef712c1210315bfae96b8
thangbui258 <thangbuimanh1996@gmail.com>
2022-07-13 09:27:20 +0700

    update

--- model/Database.js
+++ model/Database.js
@@ -3,12 +3,20 @@ let mysql = require('mysql');
 class Database{
     constructor(){
     }
+    // connect(){
+    //     return mysql.createConnection({
+    //         host: "127.0.0.1",
+    //         user: "root",
+    //         password: "25081996",
+    //         database: "QuanLyHS"
+    //     })
+    // }
     connect(){
-        return mysql.createConnection({
-            host: "127.0.0.1",
-            user: "root",
-            password: "25081996",
-            database: "QuanLyHS"
+        return mysql.createPool({
+            host: "us-cdbr-east-06.cleardb.net",
+            user: "b75b1d7dd622a7",
+            password: "0e33abea",
+            database: "heroku_05f8dfb1bed61es"
         })
     }
 }
